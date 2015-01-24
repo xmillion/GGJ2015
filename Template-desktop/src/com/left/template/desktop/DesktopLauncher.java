7f07e633bb987735261e1b11887e5c817122231e
willis <willfung616@hotmail.com>
2015-01-23 22:47:59 -0800

    Customize the launch parameters

--- Template-desktop/src/com/left/template/desktop/DesktopLauncher.java
+++ Template-desktop/src/com/left/template/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.left.addd.AdddGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Grid";
+		config.width = 800;
+		config.height = 600;
+		config.resizable = true;
 		new LwjglApplication(new AdddGame(), config);
 	}
 }
