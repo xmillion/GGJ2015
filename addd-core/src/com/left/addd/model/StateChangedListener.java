f9367fe37031945f1e54219ff2296c298ac4c3da
willis <willfung616@hotmail.com>
2015-02-01 16:18:15 -0800

    l

--- addd-core/src/com/left/addd/model/StateChangedListener.java
+++
@@ -1,9 +0,0 @@
-package com.left.addd.model;
-
-public interface StateChangedListener<T> {
-
-	/**
-	 * When the model's state changes, this should get called.
-	 */
-	public void OnStateChanged(T changedObject);
-}
